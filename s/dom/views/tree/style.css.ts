c61fae7da514b4858ae23f3aa256ce1fab02ff71
zenkyu <przemekgg2002@gmail.com>
2025-05-19 01:16:11 +0200

    improve: outliner dropzone hover and tree items styles

--- s/dom/views/tree/style.css.ts
+++ s/dom/views/tree/style.css.ts
@@ -6,6 +6,11 @@ sl-tree-item::part(expand-button) {
 	rotate: none;
 }
 
+sl-tree-item::part(base) {
+	min-height: 30px;
+	justify-content: center;
+}
+
 sl-icon:is(.item) {
 	padding: 0.5em;
 }
@@ -29,9 +34,30 @@ sl-tree-item:not([data-type=folder])::part(expand-button) {
 	bottom: 0;
 }
 
+sl-tree-item {
+	min-height: 30px;
+	transition: background 0.2s ease, border-radius 0.2s ease;
+
+	::part(label) {
+		gap: 0.5em;
+	}
+
+	.folder {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	::part(item--selected) {
+		background: rgba(0, 0, 0, 0.4);
+		min-height: 30px;
+	}
+
+}
+
 sl-tree-item[data-hover] {
 	border-radius: 5px;
-	border: 1px solid var(--sl-color-primary-600);
+	background: rgba(0, 0, 0, 0.4);
 }
 
 `
